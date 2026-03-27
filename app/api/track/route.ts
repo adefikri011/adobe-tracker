526c5646dda3db6d04fb73607e1bd081d2728fe6
Ade Fikri <fikriade257@gmail.com>
2026-03-27 09:54:47 +0700

    doneee

--- app/api/track/route.ts
+++ app/api/track/route.ts
@@ -1,7 +1,5 @@
 import { NextResponse } from 'next/server';
-
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+import { prisma } from '../../lib/prisma';
 
 export const dynamic = 'force-dynamic';
 
