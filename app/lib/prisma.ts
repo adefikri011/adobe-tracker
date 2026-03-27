8a7ea97f87d968a0f7cbc1223dcc86d331fb4b8b
Ade Fikri <fikriade257@gmail.com>
2026-03-27 09:31:52 +0700

    final: stable prisma singleton with pg adapter

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,15 +1,22 @@
-import { PrismaClient } from "../../src/generated/prisma/client";
+// lib/prisma.ts
+import { PrismaClient } from "@/src/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+const pg = require('pg'); 
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
+const pool = new pg.Pool({ 
+  connectionString: process.env.DATABASE_URL 
 });
+const adapter = new PrismaPg(pool);
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+// Inisialisasi Prisma Client dengan Adapter PG
 export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient({ adapter });
+  globalForPrisma.prisma ?? 
+  new PrismaClient({ 
+    adapter 
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
